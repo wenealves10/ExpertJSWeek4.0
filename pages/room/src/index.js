46020bb051aa078955c7f75f37d197c9e66388a0
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-17 19:19:14 -0300

    data at the project 🥇

---
+++ pages/room/src/index.js
@@ -0,0 +1,10 @@
+
+const room = {
+  id: Date.now(),
+  topic: "DevBot community",
+};
+
+const user = {
+  img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png",
+  username: "Wene Alves",
+};
